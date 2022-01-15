1b086eb8d9b1ca1505bf123565a7949603c7fbc5
Romir Arya <52969152+romir-arya12@users.noreply.github.com>
2022-01-15 12:55:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask,jsonify,request
+from storage import all_movies, liked_movies, not_liked_movies, did_not_watch
+from demographic_filtering import output
+from content_filtering import get_recommendations
+
+app=Flask(__name__)
+@app.route("/get-movie")
+def get_movie():
+    movie_data={"title": all_movies[0][19], "poster_link": all_movies[0][27], "release_date": all_movies[0][13] or "N/A", "duration": all_movies[0][15], "rating": all_movies[0][20], "overview": all_movies[0][9]}
+    return jsonify({
+        "data":movie_data,
+        "status":"success"
+    })
+
+@app.route("/liked-movie",methods=["POST"])
+def liked-movie():
+    movie=all_movies[0]
+    all_movies=all_movies[1:]
+    liked_movies.append(movie)
+    all_movies.pop(0)
+    return jsonify({
+        "status":"success"
+    }),201
+
+@app.route("/not-liked-movie",methods=["POST"])
+def unliked_movies():
+    movie=all_movies[0]
+    all_movies=all_movies[1:]
+    not_liked_movies.appened(movie)
+    all_movies.pop(0)
+    return jsonift({
+        "status":"success"
+    }),201
+
+@app.route("/did-not-watch",methods=["POST"])
+def did_not_watch():
+    move=all_movies[0]
+    all_movies=all_movies[1:]
+    did_not_watch.appened(movie)
+    all_movies.pop(0)
+    return jsonify ({
+        "status":"success"
+    }),201
+@app.route("/recommended-movies")
+def recommended_movies():
+    all_recommended=[]
+    for liked_movie in liked_movies:
+        output=get_recommendation(liked_movie[19])
+        for data in output:
+            all_recommended.append(data)
+        import itertools
+        all_recommended.sort()
+        all_recommended=list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
+        movie_data=[]
+        for recommended in all_recommended:
+            _d={"title": recommended[0], "poster_link": recommended[1], "release_date": recommended[2] or "N/A", "duration": recommended[3], "rating": recommended[4], "overview": recommended[5]}
+            movie_data.append(_d)
+        return jsonify({
+            "data":movie_data,
+            "status":success
+        }),200
+
+if __name__ == "__main__":
+    app.run()
